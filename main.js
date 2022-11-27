7d94d519820fe2f4c6c6d4f36acd84ca673cfbe1
lcar0029 <lcar0029@student.monash.edu>
2022-11-27 22:31:43 +0800

    Added hamburger menu

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 // Variables
+const menu = document.querySelector(".menu-container");
+const hamburger = document.querySelector(".collapsed-menu")
+// const settings = document.querySelector(".settings");
 
 // Media query for menu @ (width: 190px)
 let menuChildren = document.querySelector('.menu-container').children;
 console.log(menuChildren);
+
+// ============================
+// Click Events
+// ============================
+
+// Open hamburger menu
+hamburger.addEventListener('click', function() {
+    menu.classList.toggle('active');
+});
