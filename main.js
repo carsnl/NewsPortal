13c9afd538d4edc332a47e7d4e10e7ed54fa9dec
lcar0029 <lcar0029@student.monash.edu>
2022-12-03 00:25:50 +0800

    Added "Read More" button to each story. Added error page

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ const menu = document.querySelector(".menu-container");
 const hamburger = document.querySelector(".collapsed-menu")
 const searchQuery = document.querySelector(".search-query")
 const searchBtn = document.querySelector(".search-btn")
+const pageContent = document.querySelector(".page-content")
 const newsContainer = document.querySelector(".news-container")
+const newsStory = document.querySelectorAll(".news-item")
 
 // Global variables
 let endpoint = 'everything';
@@ -83,6 +85,80 @@ filterCloseBtn.addEventListener('click', function() {
 //     tagContainer.appendChild(country);
 // })
 
+function createNewsItem(data) {
+    data.articles.forEach(article => {
+        // <article class="news-item">
+        //     <img src="" alt="">
+        //     <h3></h3>
+        //     <p></p>
+        // </article>
+        let story = document.createElement('article');
+        story.classList.add('news-item')
+        // Story image
+        let img = document.createElement('img')
+        img.setAttribute('src', article.urlToImage)
+        img.setAttribute('alt', 'image')
+
+        // Story title
+        let a = document.createElement('a');
+        a.setAttribute('href', article.url);
+        a.setAttribute('target', '_blank')
+        a.textContent = article.title;
+
+        // Story description
+        let desc = document.createElement('p');
+        desc.textContent = article.description;
+
+        // Source
+        let source = document.createElement('h5')
+        source.textContent = article.source.name;
+        source.classList.add('news-source')
+
+        // Date
+        let date = document.createElement('h5')
+        date.textContent = article.publishedAt.slice(0,10);
+        date.classList.add('news-date')
+
+        // Read more button
+        let readMoreBtn = document.createElement('a')
+        readMoreBtn.innerText = 'Read More'
+
+        readMoreBtn.setAttribute('href', article.url);
+        readMoreBtn.setAttribute('target', '_blank');
+
+        readMoreBtn.classList.add('read-more-btn')
+
+        // Story content (image, title, description)
+        let content = document.createElement('div')
+        content.classList.add('story-content')
+        content.appendChild(img);
+        content.appendChild(a);
+        content.appendChild(desc);
+
+
+        // Story footer (source and date)
+        let footer = document.createElement('div')
+        footer.classList.add('story-footer')
+        footer.appendChild(source);
+        footer.appendChild(date);
+
+        // Append story to news-container
+        story.appendChild(content);
+        story.appendChild(footer);
+        story.appendChild(readMoreBtn);
+
+        // Mouseover event for 'Read More' button
+        story.addEventListener('mouseover', function() {
+            readMoreBtn.classList.toggle('active');
+        })
+        story.addEventListener('mouseout', function() {
+            readMoreBtn.classList.toggle('active');
+        })
+
+        newsContainer.appendChild(story);
+
+    })
+}
 // TEMP: fetch posts
 searchBtn.addEventListener('click', function(e) {
     e.preventDefault()
@@ -99,64 +175,33 @@ searchBtn.addEventListener('click', function(e) {
     // Remove previous stories
     removeAllChildNodes(newsContainer);
 
+    // Remove error messages
+    // TODO: improve code quality here
+    const noResultsError = document.querySelector('.no-results-container');
+    noResultsError.style.display = 'none';
+
     console.log(`https://newsapi.org/v2/${endpoint}${query}${filterSort}${filterLang}${filterStartDate}${filterEndDate}${filterCountry}&apiKey=${apiKey}`)
 
     fetch(`https://newsapi.org/v2/${endpoint}?q=${searchQuery.value}${filterSort}${filterLang}${filterStartDate}${filterEndDate}${filterCountry}&apiKey=${apiKey}`)
         .then((response) => response.json())
         .then((data) => {
-            console.log(data)
-            data.articles.forEach(article => {
-                // <article class="news-item">
-                //     <img src="" alt="">
-                //     <h3></h3>
-                //     <p></p>
-                // </article>
-                let story = document.createElement('article');
-                story.classList.add('news-item')
-                // Story image
-                let img = document.createElement('img')
-                img.setAttribute('src', article.urlToImage)
-                img.setAttribute('alt', 'image')
-
-                // Story title
-                let a = document.createElement('a');
-                a.setAttribute('href', article.url);
-                a.setAttribute('target', '_blank')
-                a.textContent = article.title;
-
-                // Story description
-                let desc = document.createElement('p');
-                desc.textContent = article.description;
-
-                // Source
-                let source = document.createElement('h5')
-                source.textContent = article.source.name;
-                source.classList.add('news-source')
-
-                // Date
-                let date = document.createElement('h5')
-                date.textContent = article.publishedAt.slice(0,10);
-                date.classList.add('news-date')
-
-                // Story content (image, title, description)
-                let content = document.createElement('div')
-                content.classList.add('story-content')
-                content.appendChild(img);
-                content.appendChild(a);
-                content.appendChild(desc);
-
-                // Story footer (source and date)
-                let footer = document.createElement('div')
-                footer.classList.add('story-footer')
-                footer.appendChild(source);
-                footer.appendChild(date);
-
-                // Append story to news-container
-                story.appendChild(content);
-                story.appendChild(footer);
-
-                newsContainer.appendChild(story);
-            })
+            try {
+                createNewsItem(data);
+            } catch(e) {
+                noResultsError.style.display = 'flex';
+            }
+            
+            // If no results, show no results error page
+            // const noResultsError = document.querySelector('.no-results-container');
+            // console.log(noResultsError);
+            // console.log(newsContainer.childElementCount)
+            // if (newsContainer.childElementCount === 0){
+            //     console.log('showing')
+            //     noResultsError.style.display = 'flex';
+            // } else if (newsContainer.childElementCount > 0 && noResultsError.classList.contains('show')) {
+            //     console.log('hiding')
+            //     noResultsError.style.display = 'none';
+            // }
         })
 
     // Clear search field
@@ -200,7 +245,7 @@ let scrollToTopBtn = document.getElementById('scroll-to-top-btn')
 // Shows a button that allows user to scroll back to top of the page
 function showScrollToTopBtn() {
     // body for Safari, documentElement for rest
-    if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0) {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         scrollToTopBtn.style.display = 'block';
     } else {
         scrollToTopBtn.style.display = 'none';
